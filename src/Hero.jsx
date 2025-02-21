dbe06e1f990157b0c311bdaf2043e23fcbbcbd95
Stephan Steinwall (CYBRIXX) <165468055+Stephan-Steinwall@users.noreply.github.com>
2025-02-21 13:57:16 +0530

    aa

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,22 +1,48 @@
 import React from "react";
-import {Button} from "./components/ui/button";
+import { Button } from "./components/ui/button";
 import { CarouselDemo } from "./components/base/CarouselDemo";
 
-
 function Hero() {
+  const getProducts = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/api/products", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className="hero-section py-8 px-8">
       <div className="hero-container grid grid-cols-2 rounded-md min-h-[60vh] bg-[#f4f8f9]">
         <div className="hero-content flex flex-col justify-center p-16 gap-4">
-          <span className="discount-badge inline-block rounded-full px-2 py-1 text-xs w-fit bg-[#febc26]">WEEKLY DISCOUNT</span>
-          <h1 className="hero-title text-6xl font-semibold leading-none">Premium Product Online Shop</h1>
+          <span className="discount-badge inline-block rounded-full px-2 py-1 text-xs w-fit bg-[#febc26]">
+            WEEKLY DISCOUNT
+          </span>
+          <h1 className="hero-title text-6xl font-semibold leading-none">
+            Premium Product Online Shop
+          </h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos
             suscipit est autem quia? Voluptatem?
           </p>
-          <a to="/shop" className="shop-now-btn px-4 py-2 text-white font-medium bg-black rounded-md inline-block w-fit">
+          <a
+            to="/shop"
+            className="shop-now-btn px-4 py-2 text-white font-medium bg-black rounded-md inline-block w-fit"
+          >
             Shop Now
           </a>
+          <div className="">
+            <Button onClick={() => getProducts()}>Get Products</Button>
+          </div>
         </div>
         <div className="hero-image-container relative">
           <img
@@ -25,9 +51,7 @@ function Hero() {
             className="hero-image w-full h-full object-cover"
           />
         </div>
-
       </div>
-
     </section>
   );
 }
